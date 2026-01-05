1a0617f04993247383d2336c5ab199007ae08d8d
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-05 04:07:44 -0500

    auth controller for existing user login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,4 +3,25 @@ import { hashPassword, comparePassword } from "../utils/hashPPassword.js";
 
 export const register = async (req, res, next) => {
   const client = createClient();
+
+  try {
+    await client.connect();
+    const { email, password, display_name } = req.body;
+
+    const existingUser = await client.query(
+        'SELECT id FROM users WHERE email = $1',
+        [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+        return res.status(400).json({ error: "Email already in use"})
+    }
+
+    const password_hash = await hashPassword(password);
+
+    const result = await client.query(
+      'INSERT INTO users (email, password_hash, display_name) VALUES ($1, $2, $3) RETURNING id, email, display_name',
+      [email, password_hash, display_name]
+    );
+  }
 };

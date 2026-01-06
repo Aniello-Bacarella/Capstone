d9867efa3094cc1c637e0a553ccb5b8317ceab21
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-05 21:42:29 -0500

    fixed typo in sound route handler

--- backend/routes/sounds.js
+++ backend/routes/sounds.js
@@ -1,5 +1,5 @@
 import express from "express";
-import * as soundController from "../controllers/soundController.js";
+import * as soundsController from "../controllers/soundController.js";
 import { requireAuth } from "../middleware/auth.js";
 import { validateSoundUpload } from "../middleware/validation.js";
 

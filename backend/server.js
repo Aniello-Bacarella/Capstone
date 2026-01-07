958492d9eb1d39b0d40a778f1ed913dfb2f01eed
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-07 01:54:48 -0500

    failing to connect to database

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import boardsRoutes from "./routes/boards.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(

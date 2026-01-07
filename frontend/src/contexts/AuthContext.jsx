e19b2cf49a8fbed50fab099f2bc55720b95937ce
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-06 23:24:23 -0500

    authcontext update/completion.

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -64,3 +64,11 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+};

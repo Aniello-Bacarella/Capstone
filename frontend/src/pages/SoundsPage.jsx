12a5d0a750666680e534029d1e3bca7b690ad3c7
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-08 22:46:15 -0500

    sounds page starter code

--- frontend/src/pages/SoundsPage.jsx
+++ frontend/src/pages/SoundsPage.jsx
@@ -2,4 +2,6 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export const SoundsPage = () => {};
+export const SoundsPage = () => {
+  return <div className="sounds-page">{}</div>;
+};

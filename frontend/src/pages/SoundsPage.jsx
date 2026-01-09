2248f7b2d9d5f7ccb09b88a1b23051b118ebc821
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-08 23:27:24 -0500

    header on sounds page

--- frontend/src/pages/SoundsPage.jsx
+++ frontend/src/pages/SoundsPage.jsx
@@ -3,5 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export const SoundsPage = () => {
-  return <div className="sounds-page">{}</div>;
+  return (
+    <div className="sounds-page">
+      <header className="page-header">
+        <div>
+          <h2>My Sounds</h2>
+          <p className="page-subtitle">Manage your audio collection!</p>
+        </div>
+        <button className="btn-primary">Upload Sound</button>
+      </header>
+    </div>
+  );
 };

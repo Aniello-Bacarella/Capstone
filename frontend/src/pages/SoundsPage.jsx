ab985c99b6f3a2be56dcdffa66370db5e02bcd8a
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-08 23:39:36 -0500

    state for showing upload form on sounds page

--- frontend/src/pages/SoundsPage.jsx
+++ frontend/src/pages/SoundsPage.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export const SoundsPage = () => {
+  const [showUploadForm, setShowUploadForm] = useState(false);
+
   return (
     <div className="sounds-page">
       <header className="page-header">
@@ -10,7 +12,12 @@ export const SoundsPage = () => {
           <h2>My Sounds</h2>
           <p className="page-subtitle">Manage your audio collection!</p>
         </div>
-        <button className="btn-primary">Upload Sound</button>
+        <button
+          className="btn-primary"
+          onClick={() => setShowUploadForm(!setShowUploadForm)}
+        >
+          {showUploadForm ? "Cancel" : "Upload Sound"}
+        </button>
       </header>
     </div>
   );

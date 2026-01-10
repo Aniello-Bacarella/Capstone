cb16acdee004766d68700f2d11ca8cc1fda6b91d
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-10 18:53:31 -0500

    set up SoundList component shell for Sounds page

---
+++ frontend/src/components/Sounds/SoundList.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+import { SoundCard } from "./SoundCard";
+
+export const SoundList = ({ refreshKey }) => {
+  return <div></div>;
+};

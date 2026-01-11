95211085343c332d0eb03a93c0920be11c08c9fe
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-11 03:48:16 -0500

    insert const created for boardscontroller

--- backend/controllers/boardsController.js
+++ backend/controllers/boardsController.js
@@ -2,5 +2,10 @@ export const createBoard = async (req, res, next) => {
     try {
         const { title, description, is_public } = req.body;
         const userId = req.session.userId; 
+
+        const result = await client.query(
+            `INSERT INTO boards (user_id, title, description, is_public) VALUES ($1, $2, $3, $4) RETURNING *`,
+            [userId, title, description || null, is_public || false]
+        );
     }
 };

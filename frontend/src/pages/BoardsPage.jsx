e43d1f318a2073eae38b499eafb9edda4e77a4b3
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-11 18:38:58 -0500

    added api import to boardspage

--- frontend/src/pages/BoardsPage.jsx
+++ frontend/src/pages/BoardsPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { api } from "../services/api";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 

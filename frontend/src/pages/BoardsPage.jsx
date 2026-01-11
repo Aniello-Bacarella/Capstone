597f495b8e027c0f9bf40785e108b32931486965
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-11 17:18:55 -0500

    state declarations for BoardsPage

--- frontend/src/pages/BoardsPage.jsx
+++ frontend/src/pages/BoardsPage.jsx
@@ -2,4 +2,14 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export const BoardsPage = () => {};
+export const BoardsPage = () => {
+  const [boards, setBoards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showCreateform, setShowCreateForm] = useState(true);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
+  const [error, setError] = useState("");
+
+  return <div>BoardsPage</div>;
+};

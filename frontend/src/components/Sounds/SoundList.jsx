3187b443a220aea3bb1f086554447e7f3e783212
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-10 20:10:02 -0500

    added const for handle update

--- frontend/src/components/Sounds/SoundList.jsx
+++ frontend/src/components/Sounds/SoundList.jsx
@@ -29,30 +29,40 @@ export const SoundList = ({ refreshKey }) => {
   };
 
   const handleDelete = (id) => {
-    setSounds(sounds.filter(s => s.id !== id));
+    setSounds(sounds.filter((s) => s.id !== id));
+  };
+
+  const handleUpdate = (sound) => {
+    const newTitle = prompt("Enter new title:".sound.title);
+    if (newTitle && newTitle !== sound.title) {
+      api
+        .updateSound(sound.id, { title: newTitle })
+        .then(() => fetchSounds())
+        .catch((err) => alert("failed to update sound"));
+    }
   };
 
   return (
-  
-  <div className= "sounds-list">
-    <div className="search-bar">
+    <div className="sounds-list">
+      <div className="search-bar">
         <input
-            type="text"
-            placeholder="search sounds"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            />
-  </div>
-
-  <div className="sounds-grid">
-    {sounds.map(sound => (
-        <SoundCard 
-        key={sound.id}
-        sound={sound}
-        onDelete={handleDelete}
-        onUpdate={handleUpdate}
+          type="text"
+          placeholder="search sounds"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-    ))}
-  </div>
-);
+      </div>
+
+      <div className="sounds-grid">
+        {sounds.map((sound) => (
+          <SoundCard
+            key={sound.id}
+            sound={sound}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };

ff28f19f09c75de28632affa33e9114f97f8db09
Aniello-Bacarella <aniello.bacarella@gmail.com>
2026-01-10 19:38:10 -0500

    added const to soundlist

--- frontend/src/components/Sounds/SoundList.jsx
+++ frontend/src/components/Sounds/SoundList.jsx
@@ -3,5 +3,9 @@ import { api } from "../../services/api";
 import { SoundCard } from "./SoundCard";
 
 export const SoundList = ({ refreshKey }) => {
+  const [sounds, setSounds] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
   return <div></div>;
 };

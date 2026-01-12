9d70506bf085c4f4252595f9989c20049134f65e
Aniello Bacarella <aniello.bacarella@gmail.com>
2026-01-12 13:50:03 -0500

    cors updates

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,11 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
-  })
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+})
 );
+
 
 const PgStore = pgSession(session);
 const pgPool = new pg.Pool({
